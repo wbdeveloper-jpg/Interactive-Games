59dfc8d51d89e58bb110b39fe0e3770903dd70de
wbdeveloper-jpg <wbdeveloper@narayanagroup.com>
2026-04-18 16:47:36 +0530

    Update Experiments.cs

--- Assets/Experiments/Experiments.cs
+++ Assets/Experiments/Experiments.cs
@@ -5,37 +5,5 @@ using UnityEngine.UIElements;
 
 public class Experiments : MonoBehaviour
 {
-    int LargestSubString(string str, int k)
-    {
-        Dictionary<char, int> tracker = new Dictionary<char, int>();
-        int left = 0;
-        int longest = 0;
-
-        for (int right = 0; right < str.Length; right++)
-        {
-            int localLongest = 0;
-            if(tracker.ContainsKey(str[right]))
-                    tracker[str[right]]++;
-            else
-                tracker.Add(str[right], 1);
-
-            
-                while (tracker[str[right]] > k)
-                {
-                    tracker[str[left]]--;
-                    if(tracker[str[left]] == 0)
-                        tracker.Remove(str[left]);
-                    left++;
-                }
-                
-                
-            
-            
-                localLongest = right - left + 1;
-
-            longest = Mathf.Max(longest, localLongest);
-        }
-
-        return longest;
-    }
+    
 }
